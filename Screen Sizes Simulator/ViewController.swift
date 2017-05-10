3c3467bc53ecb1978edb289fdfb62fd9001ab4a1
Felipe Augusto Sviaghin Ferri <felipe.ferri@gmail.com>
2017-05-10 11:29:12 -0300

    Initial Commit

---
+++ Screen Sizes Simulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Screen Sizes Simulator
+//
+//  Created by Felipe Augusto Sviaghin Ferri on 10/5/17.
+//  Copyright © 2017 Felipe Augusto Sviaghin Ferri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
